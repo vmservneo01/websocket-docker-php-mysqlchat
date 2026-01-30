ae0b2d785a688ae73053228c075bacfa76429f14
vmserv01 <nikkozyakov5@gmail.com>
2026-01-30 18:27:54 +0300

    websockchat fixed version for YandexCloud

--- chat.sh
+++ chat.sh
@@ -4,6 +4,8 @@
 # Tested on Ubuntu 22.04, 24.04
 # Developed by vagrantservernode-01 in 2025
 #--------------------------------------------------------------------
+SERVERIP=""
+
 
 ###install packages
 sudo apt update && apt upgrade
@@ -23,15 +25,23 @@ sudo apt install git -y              #git
 ###git clone
 git clone https://github.com/vmservneo01/websocket-docker-php-mysqlchat.git
 cd  websocket-docker-php-mysqlchat
+
+
+#host name
+sudo echo "$SERVERIP websockchat.ru" | sudo tee -a /etc/hosts
+
+
+
+
 ### use docker-compose for build image
 #sudo chmod +x /usr/local/bin/docker-compose 
 #sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose #access from anywhere
 #sudo rm -rf ./.data/db/*
-#docker-compose build
-#docker-compose up -d
-#docker-compose logs nginx >> ./logs/docker-compose/logs_nginx
-#docker-compose logs apache >> ./logs/docker-compose/logs_apache
-#docker-compose logs apache >> ./logs/docker-compose/logs_mysql
+docker-compose build
+docker-compose up -d
+docker-compose logs nginx >> ./logs/docker-compose/logs_nginx
+docker-compose logs apache >> ./logs/docker-compose/logs_apache
+docker-compose logs apache >> ./logs/docker-compose/logs_mysql
 
 ### setup firewall
 #sudo apt install ufw -y
